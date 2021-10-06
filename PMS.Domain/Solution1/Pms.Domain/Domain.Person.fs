a58c568695de5ee9428eb5fc39466b506b638bbf
Dewald Haasbroek <73649603+Dewald844@users.noreply.github.com>
2021-10-06 07:33:05 +0200

    Domain.Person
    command interpret for person domain

--- PMS.Domain/Solution1/Pms.Domain/Domain.Person.fs
+++ PMS.Domain/Solution1/Pms.Domain/Domain.Person.fs
@@ -1,5 +1,6 @@
 namespace Domain
 
+open System
 open Types
 open Types.Helpers
 
@@ -12,6 +13,7 @@ module Person =
     EmailAddress    : EmailAddress
     PhysicalAddress : PhysicalAddress
   }
+
   type PersonDetails ={
     PersonId       : Int99
     Name           : FirstName * LastName
@@ -46,3 +48,71 @@ module Person =
     | TelephoneNumberUpdated of State * PersonData
     | EmailUpdated           of State * PersonData
     | AddressUpdated         of State * PersonData
+
+  type FailedEvent =
+    | CreateFailed                of State * Exception
+    | TelephoneNumberUpdateFailed of State * Exception
+    | EmailUpdateFailed           of State * Exception
+    | AddressUpdateFailed         of State * Exception
+
+  type Event = Result<SuccessFullEvent,FailedEvent>
+
+  module Command =
+
+    let interpretCommand (personState : State) (command : Command) : Event =
+      match command with
+      | Create (personType, personDetails) ->
+        match personState with
+        | NonExisting ->
+          let newPersonData =
+            match personType with
+            | PersonType.Architect  -> Architect <| personDetails
+            | PersonType.Contractor -> Contractor <| personDetails
+            | PersonType.Customer   -> Customer <| personDetails
+          Ok (SuccessFullEvent.Created <| (personState, newPersonData))
+        | Existing _ ->
+          Error (FailedEvent.CreateFailed <| (personState,($"Can not create person with an existing state REF {personState}" |> Exception)))
+      | Update method ->
+        match method with
+        | UpdateTelephoneNumber newNumber ->
+          match personState with
+          | Existing personData ->
+            match personData with
+            | Architect currentData ->
+              let newContactDetails = {currentData.ContactDetails with TelephoneNumber = newNumber }
+              let newData = {currentData with ContactDetails = newContactDetails}
+              let newPersonData = Architect <| newData
+              Ok (SuccessFullEvent.TelephoneNumberUpdated <| (personState, newPersonData))
+            | Contractor currentData ->
+              let newContactDetails = {currentData.ContactDetails with TelephoneNumber = newNumber }
+              let newData = {currentData with ContactDetails = newContactDetails}
+              let newPersonData = Contractor <| newData
+              Ok (SuccessFullEvent.TelephoneNumberUpdated <| (personState, newPersonData))
+            | Customer currentData ->
+              let newContactDetails = {currentData.ContactDetails with TelephoneNumber = newNumber }
+              let newData = {currentData with ContactDetails = newContactDetails}
+              let newPersonData = Customer <| newData
+              Ok (SuccessFullEvent.TelephoneNumberUpdated <| (personState, newPersonData))
+          | NonExisting ->
+            Error (FailedEvent.TelephoneNumberUpdateFailed <| (personState, ($"Can not update an non existing person's telephone number REF {personState}" |> Exception)))
+        | UpdateEmail newEmail ->
+          match personState with
+          | Existing personData ->
+            match personData with
+            | Architect currentData ->
+              let newContactDetails = {currentData.ContactDetails with EmailAddress = newEmail }
+              let newData = {currentData with ContactDetails = newContactDetails}
+              let newPersonData = Architect <| newData
+              Ok (SuccessFullEvent.EmailUpdated <| (personState, newPersonData))
+            | Contractor currentData ->
+              let newContactDetails = {currentData.ContactDetails with EmailAddress = newEmail }
+              let newData = {currentData with ContactDetails = newContactDetails}
+              let newPersonData = Contractor <| newData
+              Ok (SuccessFullEvent.EmailUpdated <| (personState, newPersonData))
+            | Customer currentData ->
+              let newContactDetails = {currentData.ContactDetails with EmailAddress = newEmail }
+              let newData = {currentData with ContactDetails = newContactDetails}
+              let newPersonData = Architect <| newData
+              Ok (SuccessFullEvent.EmailUpdated <| (personState, newPersonData))
+          | NonExisting ->
+            Error (FailedEvent.EmailUpdateFailed <| (personState, ($"Can not update an non existing person's Email REF {personState}" |> Exception)))
